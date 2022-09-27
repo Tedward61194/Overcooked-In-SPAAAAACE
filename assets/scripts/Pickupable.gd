099622436fdf204083be816f9091386a95dd1330
Ted Segal <tedward61194@gmail.com>
2022-09-27 16:13:15 -0400

    Refactored pick up and put down logic

--- assets/scripts/Pickupable.gd
+++ assets/scripts/Pickupable.gd
@@ -6,10 +6,13 @@ class_name Pickupable extends Node3D
 @export var pickup_scene : PackedScene
 
 
-func pickup(handPosition : Node3D):
+func pickup(handPosition : Node3D) -> int:
 	if pickup_scene:
 		var inatance = pickup_scene.instantiate()
 		handPosition.add_child(inatance)
 		inatance.set_global_position(handPosition.get_global_position())
 		root.queue_free()
+		return id
+	push_error("Failed to get pickup_scene")
+	return -1
 
--- assets/scripts/Player/Player.gd
+++ assets/scripts/Player/Player.gd
@@ -6,8 +6,15 @@ const SPEED := 5.0
 # config
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+# ref
+@onready var pick_up_position := $Pivot/BodyMesh/Hand/PickUpPosition
+@onready var hand_reach_ray := $Pivot/BodyMesh/HandsReach
+@onready var feet_reach_ray := $Pivot/BodyMesh/FeetReach
+
+
 # logic
-var obj_holding = null
+var obj_in_hands : int = -1
+var hand_looking_at
 
 
 func _ready() -> void:
@@ -18,23 +25,60 @@ func _physics_process(delta) -> void:
 	process_movement(delta)
 
 
+func _process(_delta):
+	check_hand_reach_ray()
+
+
+func _input(_event):
+	if Input.is_action_just_pressed("PickUp"):
+		pick_up_put_down()
+
+
+
 func process_movement(delta) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-		
+
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
 
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		
+
 		direction = direction.normalized()
 		$Pivot.look_at(position + direction, Vector3.UP)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
+
 	move_and_slide()
+
+
+
+func check_hand_reach_ray():
+	# Look for hilightable node in front of player
+	# If found highlight it
+	# Update hand_looking_at
+	var col = hand_reach_ray.get_collider()
+	if col != hand_looking_at:
+		if col and col.has_node("Highlightable"):
+			col.get_node("Highlightable").targeted = true
+		if hand_looking_at and hand_looking_at.has_node("Highlightable"):
+			hand_looking_at.get_node("Highlightable").targeted = false
+		hand_looking_at = col
+
+
+func pick_up_put_down():
+	if obj_in_hands == -1: # Hands empty. Try pick up
+		if hand_looking_at != null and hand_looking_at.has_node("Pickupable"):
+			obj_in_hands = hand_looking_at.get_node("Pickupable").pickup(pick_up_position)
+	else: # Try put down
+		var col = feet_reach_ray.get_collider()
+		if col.has_node("PlacementSlot"):
+			var put_down = col.get_node("PlacementSlot").try_put_down(obj_in_hands)
+			if put_down:
+				obj_in_hands = -1
+				pick_up_position.get_child(0).queue_free() # pickup method attached obj as child
+
